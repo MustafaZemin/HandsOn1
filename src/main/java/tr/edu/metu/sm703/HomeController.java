4b09539e58478b1478ecab3bae3ba8c9cc8ccea9
MustafaZemin <76277936+MustafaZemin@users.noreply.github.com>
2024-05-25 21:05:08 +0300

    Update HomeController.java

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -2,6 +2,7 @@ package tr.edu.metu.sm703;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -9,13 +10,9 @@ public class HomeController {
 
     @Get
     public Map<String, Object> index() {
-        int res = sum(29, 49);
-        //System.out.println(res);
-        return Collections.singletonMap("message", res);
-    }
-
-    public static int sum(int num1, int num2)
-    {
-        return num1+num2;
+        Map map = new HashMap();
+        map.put("statusCode", 200);
+        map.put("body", "{\"message\", \"Hello AWS World\"}");
+        return map;
     }
 }
