a651d1e9d7bad46d7223b17102b55ece0372a9d4
MustafaZemin <76277936+MustafaZemin@users.noreply.github.com>
2024-05-26 20:28:30 +0300

    Update HomeController.java

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -1,18 +1,14 @@
 package tr.edu.metu.sm703;
+
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import io.micronaut.http.HttpStatus;
 
-@Controller
+@Controller("/")
 public class HomeController {
 
-    @Get
-    public Map<String, Object> index() {
-        Map map = new HashMap();
-        map.put("statusCode", 200);
-        map.put("body", "{\"message\", \"Hello AWS World\"}");
-        return map;
+    @Get("/")
+    public String index() {
+        return "Hello World";
     }
 }
